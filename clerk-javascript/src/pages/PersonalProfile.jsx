a791145d725d0e699a6e4c1b8066b2eeb8bab121
Dylan Luc <122325393+nguydluc@users.noreply.github.com>
2024-11-09 16:01:52 -0700

    Initial Commit

---
+++ clerk-javascript/src/pages/PersonalProfile.jsx
@@ -0,0 +1,5 @@
+function PersonalProfile() {
+  return <div></div>;
+}
+
+export default PersonalProfile;
--- clerk-javascript/tailwind.config.js
+++ clerk-javascript/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensures Tailwind scans your files for classes
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};

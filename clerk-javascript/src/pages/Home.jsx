3afaca9d542736a82108bd0b1cfb6d66cd0fb559
bnhao <88442790+bnhao@users.noreply.github.com>
2024-11-10 11:14:44 -0700

    update Home.jsx

--- clerk-javascript/src/pages/Home.jsx
+++ clerk-javascript/src/pages/Home.jsx
@@ -1,22 +1,39 @@
-import { SignInButton, SignedOut } from "@clerk/clerk-react";
+import { SignInButton, SignedOut, SignedIn, useUser } from "@clerk/clerk-react";
 import Cat from "../assets/cat.svg";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function Home() {
+  const { isSignedIn } = useUser(); // Clerk hook to check if the user is signed in
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      navigate("/dailywork");
+    }
+  }, [isSignedIn, navigate]);
+
   return (
-    <SignedOut>
-      <main className="bg-[#F79B3A] h-[100vh] pt-10">
-        <div className="max-w-[500px] flex flex-col justify-center items-center mx-auto p-4">
-          <img className="w-[150px] h-auto" src={Cat} alt="howToMeow's logo" />
-          <div className="py-[10vh] w-full">
-            <div className="">
-              <SignInButton className="bg-black text-white text-sm rounded-lg block w-full p-2.5">
-                Get Started
-              </SignInButton>
+    <>
+      <SignedOut>
+        <main className="bg-[#F79B3A] h-[100vh] pt-10">
+          <div className="max-w-[500px] flex flex-col justify-center items-center mx-auto p-4">
+            <img className="w-[150px] h-auto" src={Cat} alt="howToMeow's logo" />
+            <div className="py-[10vh] w-full">
+              <div className="">
+                <SignInButton className="bg-black text-white text-sm rounded-lg block w-full p-2.5">
+                  Get Started
+                </SignInButton>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-    </SignedOut>
+        </main>
+      </SignedOut>
+
+      <SignedIn>
+        {/* Empty JSX here as the useEffect handles the redirection */}
+      </SignedIn>
+    </>
   );
 }
 
